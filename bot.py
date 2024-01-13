ac4e8ef0838766b4595d0c3a3761948d62f33c9e
rtnkv <rtnkv@yandex.ru>
2024-01-13 20:21:40 +0200

    optimizing bot

--- bot.py
+++ bot.py
@@ -3,12 +3,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 from dotenv import load_dotenv
 import os
-from config import RSS_URL
 from rss_parser import load_rss_sources, get_all_news
-
-
-RSS_URLS = load_rss_sources('rss_sources.json')
-
 
 # Загрузка конфигурации из .env файла
 load_dotenv()
@@ -29,19 +24,15 @@ async def send_welcome(message: types.Message):
 
 @dp.message(Command(commands=['news']))
 async def send_news(message: types.Message):
-    # Здесь будет логика для получения и отправки новостей
-    await message.answer("Здесь будет список последних новостей.")
+    # Получение и отправка новостей
+    RSS_URLS = load_rss_sources('rss_sources.json')
+    news_items = get_all_news(RSS_URLS)
+    for item in news_items[:5]:  # Ограничение количества новостей для отправки
+        await message.answer(f"{item['title']}\n{item['link']}")
 
 async def main():
     # Запуск бота
     await dp.start_polling(bot)
 
-@router.message(Command(commands=['news']))
-async def send_news(message: types.Message):
-    news_items = get_all_news(RSS_URLS)
-    for item in news_items[:5]:  # Ограничение количества новостей для отправки
-        await message.answer(f"{item['title']}\n{item['link']}")
-
 if __name__ == '__main__':
     asyncio.run(main())
-
