add6bebef786ae6c0cb026433243eaf40c25a39d
rtnkv <rtnkv@yandex.ru>
2024-01-13 19:24:25 +0200

    bot.py edits

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import logging
 import asyncio
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, Router, types
 from dotenv import load_dotenv
 import os
 
@@ -11,24 +11,26 @@ API_TOKEN = os.getenv('TELEGRAM_API_TOKEN')
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
 
-# Инициализация бота и диспетчера
+# Инициализация бота и роутера
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher()
+router = Router()
 
-@dp.message_handler(commands=['start', 'help'])
+@router.message(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
     """
     Этот хендлер будет вызван, когда пользователь отправит команду /start или /help
     """
     await message.reply("Привет! Я бот, который предоставляет новости. Отправьте мне команду /news, чтобы получить последние новости.")
 
-@dp.message_handler(commands=['news'])
+@router.message(commands=['news'])
 async def send_news(message: types.Message):
     # Здесь будет логика для получения и отправки новостей
     await message.reply("Здесь будет список последних новостей.")
 
 async def main():
-    await dp.start_polling()
+    dp = Dispatcher()
+    dp.include_router(router)
+    await dp.start_polling(bot)
 
 if __name__ == '__main__':
     asyncio.run(main())
