d39361ff1d35c0fb121825ed2a374b317139cc6f
rtnkv <rtnkv@yandex.ru>
2024-01-13 19:22:57 +0200

    bot.py edits

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import logging
-from aiogram import Bot, Dispatcher
-from aiogram.types import Message
-from aiogram.utils.executor import start_polling
+import asyncio
+from aiogram import Bot, Dispatcher, types
 from dotenv import load_dotenv
 import os
 
@@ -14,20 +13,23 @@ logging.basicConfig(level=logging.INFO)
 
 # Инициализация бота и диспетчера
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher()
 
 @dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: Message):
+async def send_welcome(message: types.Message):
     """
     Этот хендлер будет вызван, когда пользователь отправит команду /start или /help
     """
     await message.reply("Привет! Я бот, который предоставляет новости. Отправьте мне команду /news, чтобы получить последние новости.")
 
 @dp.message_handler(commands=['news'])
-async def send_news(message: Message):
+async def send_news(message: types.Message):
     # Здесь будет логика для получения и отправки новостей
     await message.reply("Здесь будет список последних новостей.")
 
+async def main():
+    await dp.start_polling()
+
 if __name__ == '__main__':
-    start_polling(dp, skip_updates=True)
+    asyncio.run(main())
 
