96c7036204a75143413cb9460a91b7efdcee2b18
rtnkv <rtnkv@yandex.ru>
2024-01-13 19:18:42 +0200

    bot.py edits

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 import logging
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.utils.executor import start_polling
 from dotenv import load_dotenv
 import os
 
@@ -13,20 +16,20 @@ logging.basicConfig(level=logging.INFO)
 # Инициализация бота и диспетчера
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
+dp.middleware.setup(LoggingMiddleware())
 
 @dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message):
+async def send_welcome(message: Message):
     """
     Этот хендлер будет вызван, когда пользователь отправит команду /start или /help
     """
     await message.reply("Привет! Я бот, который предоставляет новости. Отправьте мне команду /news, чтобы получить последние новости.")
 
 @dp.message_handler(commands=['news'])
-async def send_news(message: types.Message):
+async def send_news(message: Message):
     # Здесь будет логика для получения и отправки новостей
     await message.reply("Здесь будет список последних новостей.")
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
-
+    start_polling(dp, skip_updates=True)
 
